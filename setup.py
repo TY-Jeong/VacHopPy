df4b91106df5097141982f0059ba5f2f37c8de30
정태영 <helianthus312@gmail.com>
2025-03-20 16:21:20 +0900

    mac

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vachoppy',
-    version='0.3.17',
+    version='0.3.20',
     packages = find_packages(),
     entry_points={
         'console_scripts': [
